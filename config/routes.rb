9496e0468255b21b980e82fe018827ae980ba51e
platrol <takialog@gmail.com>
2018-03-21 14:00:47 +0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update] do
+
+  end
 
   root 'posts#index'
+
 end
